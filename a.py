12b2db1ffafe3f037d94f08e4868013315a38cc4
hkitw <58417846+hkitw@users.noreply.github.com>
2019-12-02 16:41:31 +0800

    Update a.py
    V2

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 def hi(name):
     print('Hi ' + name + '!')
 
-girls = ['Michelle', 'Ola', 'Mosky', 'Phoebe', 'you']
+girls = ['Michelle', 'Ola', 'Mosky', 'Phoebe', 'David']
 
 for name in girls:
     hi(name)
